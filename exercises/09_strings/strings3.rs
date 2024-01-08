46ab6df9571bbdaa2dfa42e1a63d7a4cfc34313e
Brenden <brenden.millice@gmail.com>
2024-01-08 16:51:28 -0500

    Completed exercises 1-4

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -7,18 +7,22 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // format!("{input} world!")
+    let mut result = String::from(input);
+    result.push_str(" world!");
+    result
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
+
 
 #[cfg(test)]
 mod tests {
